6eb8772cef72505a0a6102c4c4e660b816c77520
Robert <johnidevo@gmail.com>
2022-07-11 07:30:25 +0000

    commit

--- src/Suiteziel/Org/Diamonds.php
+++ src/Suiteziel/Org/Diamonds.php
@@ -4,6 +4,19 @@ namespace App\Suiteziel\Org;
 
 class Diamonds
 {
+		
+	public function compile_contract($sFilePath = null) {
+		if(empty($sFilePath)) return print '$sFilePath missing!';
+		$sCommand = 'solc --bin-runtime --overwrite --asm --optimize -o . '.$sFilePath;
+		
+		$output=null;
+		$retval=null;
+		exec($sCommand, $output, $retval);
+		echo "Returned with status $retval and output:\n";
+		print_r($output);
+		return $output;
+	}
+	
 	public function write_to_file($sFilePath) {
 		return file_exists($sFilePath);
 	}
--- www/index.php
+++ www/index.php
@@ -1,26 +1,20 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
 require __DIR__.'/../config/bootstrap.php';
 
+/*
 use App\Suiteziel\Vm;
 use App\Suiteziel\Vm\Opcodes;
 
-var_dump(__DIR__);
 
 $oVm = new Vm();
 var_dump($oVm->run());
 
+*/
 
 use App\Suiteziel\Org\Diamonds;
-
-var_dump(file_exists($sFilePath));
 $oDiamonds = new Diamonds();
-var_dump($oDiamonds->write_to_file(__APP__));
+var_dump($oDiamonds->compile_contract(__APP__ . 'contracts/Contract.sol'));
 	
 	
 
