63e7543b1c6d70180a6e3d8dfa61949477c70131
Robert <johnidevo@gmail.com>
2022-07-13 08:57:47 +0000

    commit

--- www/index.php
+++ www/index.php
@@ -64,9 +64,11 @@ foreach ($aHexBased as $k => $sHex) {
 		$iCountArguments = $iCountArguments - 1;
 		continue;
 	}
-	print($sHex ." \t# ". $oOpcodes->describe($sHex));
-	
+
 	$oStack->initiate($k, $sHex);
+	
+	print($sHex ." \t# ". $oOpcodes->describe($sHex, $oStack->aArguments));
+	
 	/*var_dump('oStack->aArguments');
 	var_dump($oStack->aArguments);
 	var_dump(count($oStack->aArguments));
