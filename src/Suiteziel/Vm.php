649b72904115b7c6717679dedaf53a0c1c26c1d2
Robert <johnidevo@gmail.com>
2022-07-15 13:20:20 +0000

    commit

--- src/Suiteziel/Vm.php
+++ src/Suiteziel/Vm.php
@@ -2,57 +2,29 @@
 namespace App\Suiteziel;
 
 use App\Suiteziel\Vm\Box;
-use App\Suiteziel\Vm\Stack;
-use App\Suiteziel\Vm\Memory;
-use App\Suiteziel\Vm\Opcodes;
-
 
 class Vm
 {
-	public $oOpcodes;
-	public $oMemory;
-	public $oStack;
+
+	public $oBox;
 	public $aHex;
 	
 	public function __construct() {
-		$this->oOpcodes = new Opcodes();
-		$this->oMemory = new Memory();
-		$this->oStack = new Stack();
 		$this->oBox = new Box();
 	}
 	
 	public function set_hex($aHex = null) :bool {
 		if (empty($aHex)) return false;
-		//
-		if (!$this->oOpcodes->hex_set($aHex)) die('oOpcodes->hex_set');
-		//
 		$this->aHex = $aHex;
 		return true;
 	}
 	
 	// loop
 	public function run () {
-		$iCountArguments = 0; //box pc var
+		
+		if (!$this->oBox->hex_set($this->aHex)) die('oBox->hex_set');
+		if (!$this->oBox->implement()) die('oBox->implement');
 
-		foreach ($this->aHex as $k => $sHex) {
-			if ($iCountArguments !== 0) {
-				$iCountArguments = $iCountArguments - 1;
-				continue;
-			}
-
-			if (!$this->oOpcodes->initiate($k, $sHex)) die('oOpcodes->initiate');
-			if (!$this->oOpcodes->describe($k, $sHex)) die('oOpcodes->describe');
-			$iCountArguments = count($this->oOpcodes->aArguments);
-
-/*
-1. set_oo variables
-2. instantiate in second layer
-3. 
-*/
-
-			if (!$this->oBox->implement($k, $sHex)) die('oBox->implement');
-
-		}
 		
 	}
 
--- src/Suiteziel/Vm/Box.php
+++ src/Suiteziel/Vm/Box.php
@@ -1,13 +1,20 @@
 <?php
 namespace App\Suiteziel\Vm;
 
+use App\Suiteziel\Vm\Opcodes;
+use App\Suiteziel\Vm\Stack;
+
 
 class Box
 {
 	public $i_pc; //program counter
 	public $aArguments;
+	public $aHex;
 
 	function __construct() {
+		$this->oOpcodes = new Opcodes();
+		$this->oMemory = new Memory();
+		$this->oStack = new Stack();
 	}
 	
 	public function new_from_call($sJson = null, $aParameters = null) {
@@ -30,18 +37,59 @@ class Box
 	public function push () {
 		array_unshift($oStack->aaStack, array_reverse($oStack->aArguments));
 	}
+		
+	public function hex_set ($aHex = null) :bool {
+		$this->aHex = $aHex;
+		return true;
+	}
 	
-	public function implement ($iKey = null): bool { // $aArguments = null, 
+	public function implement ($iKey = null, $sHex = null): bool { // $aArguments = null, 
+		
+		
+		if (!$this->oOpcodes->hex_set($this->aHex)) die('oOpcodes->hex_set');
+		
+		$iCountArguments = 0; //box pc var
+		foreach ($this->aHex as $k => $sHex) {
+			if ($iCountArguments !== 0) {
+				$iCountArguments = $iCountArguments - 1;
+				continue;
+			}
+
+			if (!$this->oOpcodes->initiate($k, $sHex)) die('oOpcodes->initiate');
+			if (!$this->oOpcodes->describe($k, $sHex)) die('oOpcodes->describe');
+			
+			//if (!$this->oBox->implement($k, $sHex)) die('oBox->implement');
+			
+			$iCountArguments = count($this->oOpcodes->aArguments);
+
+
+		}
+		
+		
+		
+		//if (!$this->oOpcodes->initiate($k, $sHex)) die('oOpcodes->initiate');
+		//if (!$this->oOpcodes->describe($k, $sHex)) die('oOpcodes->describe');
+/*
+
+if (!$this->oOpcodes->initiate($iKey, $sHex)) die('oOpcodes->initiate');
+if (!$this->oOpcodes->describe($iKey, $sHex)) die('oOpcodes->describe');
+
+//if (!$this->oBox->implement($k, $sHex)) die('oBox->implement');
+
+$this->aArguments = count($this->oOpcodes->aArguments);
+*/
+
 		//$this->aArguments = $aArguments;
 		//$oStack->aArguments = '#######';
-		case 4: return $this->aaOpcodes[$sHex][3]; //name
-		case 5: return $this->aaOpcodes[$sHex][4]; //description
-		
+		//case 4: return $this->aaOpcodes[$sHex][3]; //name
+		//case 5: return $this->aaOpcodes[$sHex][4]; //description
+		/*
 		switch ($iKey) {
 			case 0x60: 
 				return array_unshift($this->oStack->aaStack, array_reverse($this->oStack->aArguments));
 				break; //ADD
 		}
+		*/
 		
 		return true;
 	}
