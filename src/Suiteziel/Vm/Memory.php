6e07e67cf0f0fad2d3669aaffaac8f3722b13144
Robert <johnidevo@gmail.com>
2022-07-18 08:24:25 +0000

    commit

--- src/Suiteziel/Vm/Memory.php
+++ src/Suiteziel/Vm/Memory.php
@@ -6,9 +6,12 @@ use App\Suiteziel\Vm\Box;
 
 class Memory extends Box
 {
+	public $aaMemory;
+	
 	public function __construct () {
-		
+		$this->aaMemory = array();
 	}
+	
 	public function test() {
 		return  "Hello woorld Memory";
 	}
--- src/Suiteziel/Vm/Storage.php
+++ src/Suiteziel/Vm/Storage.php
@@ -6,9 +6,12 @@ use App\Suiteziel\Vm\Box;
 
 class Storage extends Box
 {
+	public $aaStorage;
+	
 	public function __construct () {
-		
+		$this->aaStorage = array();
 	}
+	
 	public function test() {
 		return  "Hello woorld Storage";
 	}
