f96fb15bfeee326316ffe9c33e0cb6752175f4b1
Robert <johnidevo@gmail.com>
2022-07-18 08:34:06 +0000

    commit

--- pear.sh
+++ pear.sh
@@ -3,7 +3,9 @@
 #wget http://pear.php.net/go-pear.phar
 #php go-pear.phar
 
-sudo apt update
+#sudo apt update
 #sudo apt install php-ds
 
-sudo apt install php-mbstring
+#sudo apt install php-mbstring
+
+composer require bariew/yii2-node-tree
