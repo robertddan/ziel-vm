157ede7cee29df22e5e2fa3553a453a328b624d4
Robert <johnidevo@gmail.com>
2022-07-22 14:20:17 +0000

    message

--- src/Suiteziel/Org/Session.php
+++ src/Suiteziel/Org/Session.php
@@ -4,18 +4,25 @@ namespace App\Suiteziel\Org;
 
 class Session
 {
-
+	public $sPath;
+	
 	public function __construct () {
-		$this->aConfig = array(
-			'file' => OPENSSL_KEYTYPE_EC,
-			'curve_name' => 'secp256k1'
-		);
+		$this->sPath = __DIR__ .'/../.database/.session';
 	}
 	
-	public function generate_keys () :bool {
-		
+	public function new_session () :bool {
+		if (file_put_contents($this->sPath, serialize(array()))) return true;
+		else return false;
 	}
-	
+
+	public function load_session () {
+		return unserialize(file_get_contents($this->sPath));
+	}
+
+	public function save_session ($sData) {
+		if (file_put_contents($this->sPath, serialize($sData))) return true;
+		else return false;
+	}
 	
 }
 
