a835afa11ab8387b4236253f8d3e9158bdcad173
Robert <johnidevo@gmail.com>
2022-07-22 13:47:22 +0000

    message

--- src/Suiteziel/Org/Address.php
+++ src/Suiteziel/Org/Address.php
@@ -5,7 +5,7 @@ namespace App\Suiteziel\Org;
 use Sop\CryptoTypes\Asymmetric\EC\ECPublicKey;
 use Sop\CryptoTypes\Asymmetric\EC\ECPrivateKey;
 use Sop\CryptoEncoding\PEM;
-use kornrunner\keccak;
+use kornrunner\Keccak;
 
 class Address
 {
@@ -25,43 +25,41 @@ class Address
 	
 	public function generate_keys () {
 		// Generate Private Key
-		openssl_pkey_export($this->oOpaque, $sKeyPrivate);
+		openssl_pkey_export($this->oOpaque, $prev_key);
 
 		// Get The Public Key
-		$key_detail = openssl_pkey_get_details($sKeyPrivate);
-		$pub_key = $key_detail;
+		//        $key_detail = openssl_pkey_get_details($this->oOpaque);
+		//        $pub_key = $key_detail["key"];
+		$priv_pem = PEM::fromString($prev_key);
 
-		$priv_pem = PEM::fromString($priv_key);
-		
-		
-// Convert to Elliptic Curve Private Key Format
-$ec_priv_key = ECPrivateKey::fromPEM($priv_pem);
+		// Convert to Elliptic Curve Private Key Format
+		$ec_priv_key = ECPrivateKey::fromPEM($priv_pem);
 
-// Then convert it to ASN1 Structure
-$ec_priv_seq = $ec_priv_key->toASN1();
+		// Then convert it to ASN1 Structure
+		$ec_priv_seq = $ec_priv_key->toASN1();
 
-// Private Key & Public Key in HEX
-$priv_key_hex = bin2hex($ec_priv_seq->at(1)->asOctetString()->string());
-$priv_key_len = strlen($priv_key_hex) / 2;
-$pub_key_hex = bin2hex($ec_priv_seq->at(3)->asTagged()->asExplicit()->asBitString()->string());
-$pub_key_len = strlen($pub_key_hex) / 2;
+		// Private Key & Public Key in HEX
+		$priv_key_hex = bin2hex($ec_priv_seq->at(1)->asOctetString()->string());
+		$pub_key_hex = bin2hex($ec_priv_seq->at(3)->asTagged()->asExplicit()->asBitString()->string());
 
-// Derive the Ethereum Address from public key
-// Every EC public key will always start with 0x04,
-// we need to remove the leading 0x04 in order to hash it correctly
-$pub_key_hex_2 = substr($pub_key_hex, 2);
-$pub_key_len_2 = strlen($pub_key_hex_2) / 2;
 
-// Hash time
-$hash = Keccak::hash(hex2bin($pub_key_hex_2), 256);
+		// Derive the Ethereum Address from public key
+		// Every EC public key will always start with 0x04,
+		// we need to remove the leading 0x04 in order to hash it correctly
+		$pub_key_hex_2 = substr($pub_key_hex, 2);
 
-// Ethereum address has 20 bytes length. (40 hex characters long)
-// We only need the last 20 bytes as Ethereum address
-$wallet_address = '0x' . substr($hash, -40);
-$wallet_private_key = '0x' . $priv_key_hex;
 
-echo "\r\n   ETH Wallet Address: " . $wallet_address;
-echo "\r\n   Private Key: " . $wallet_private_key;
+		// Hash time
+		$hash = Keccak::hash(hex2bin($pub_key_hex_2), 256);
+
+
+		// Ethereum address has 20 bytes length. (40 hex characters long)
+		// We only need the last 20 bytes as Ethereum address
+		$wallet_address = '0x' . substr($hash, -40);
+		$wallet_private_key = '0x' . $priv_key_hex;
+		$wallet_pubic_key = '0x' . $pub_key_hex;
+
+		return array("address" => $wallet_address, "private_key" => $wallet_private_key, "pubic_key" => $wallet_pubic_key);
 		
 		
 	}
