c01e72744184f4e35871af27cd3ec6dec7eb67ff
Robert-Dan <danrobert445@gmail.com>
2022-08-08 10:43:29 +0000

    message

--- src/Suiteziel/Org/Transport.php
+++ src/Suiteziel/Org/Transport.php
@@ -8,18 +8,6 @@ class Transport
 
 	}
 
-	public function rpc_dns () {
-
-	}
-
-	public function p2p_overlay () {
-
-	}
-
-	public function api () {
-
-	}
-	
 	private function rpc_client(o){}
 	private function rpc_server(o){}
 }
