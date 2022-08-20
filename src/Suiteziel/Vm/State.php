f5eeff9aaa28ebb46befa09c30c9d01797d7077f
Replit user <>
2022-08-20 17:45:55 +0000

    message

--- src/Suiteziel/Vm/State.php
+++ src/Suiteziel/Vm/State.php
@@ -83,7 +83,7 @@ class State
 				$a_e = array_splice(Stack::$aaStack, 0, $iDelta);
 				foreach($a_e as &$s_x) $s_x = hexdec($s_x);
 				$i_pc = $a_e[0] - 1;
-				print("i_pc: ". $i_pc + 1);
+				print("i_pc: ".( $i_pc + 1));
 				print(PHP_EOL);
 				print("Stack::". implode("::", Stack::$aaStack));
 				print(PHP_EOL);
@@ -95,7 +95,7 @@ class State
 				$iResult = gmp_cmp($oaFor, 0);
 				if ($iResult !== 0) $i_pc = $a_e[0] - 1;
 				else $i_pc = $i_pc;
-				print("i_pc: ". $i_pc + 1);
+				print("i_pc: ". ($i_pc + 1));
 				print(PHP_EOL);
 				print("Stack::". implode("::", Stack::$aaStack));
 				print(PHP_EOL);
