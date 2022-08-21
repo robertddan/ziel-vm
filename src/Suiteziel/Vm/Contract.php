bbbd5cb6e68c4f2a15887958899b4b86f79096fa
Replit user <>
2022-08-21 10:32:35 +0000

    message

---
+++ src/Suiteziel/Vm/Contract.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Suiteziel\Vm;
+
+
+use App\Suiteziel\Org\Session;
+/*
+use App\Suiteziel\Org\Address;
+use App\Suiteziel\Org\Database;
+use App\Suiteziel\Org\Diamonds;
+use App\Suiteziel\Org\Session;
+use App\Suiteziel\Org\Utils;
+*/
+
+class Contract
+{
+	public $aaContract;
+
+	public function __construct () {
+	}
+
+	public function contract_init () :bool {
+		return true;
+	}
+
+	public function contract_encode () :bool {
+		return true;
+	}
+  
+}
+
+?>
