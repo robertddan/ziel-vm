ffb53febad7c896cddaffd473015c7bf07ddaafe
Robert-Dan <danrobert445@gmail.com>
2022-08-21 17:39:29 +0000

    👌👌👌

--- www/index.js
+++ www/index.js
@@ -5,8 +5,8 @@ const path = require('path')
 const fs = require('fs')
 //const ziel = require('./ziel');
 
-	
-let sNetwork = 'ropsten'; //homestead
+
+let sNetwork = 'goerli'; //homestead
 let provider = new ethers.providers.InfuraProvider(sNetwork, "3e7f88ed3dc242c38332ecf58900a68e");
 //let wallet = new ethers.Wallet(ziel.sZielPrivate1, provider);
 /*
@@ -17,8 +17,8 @@ let provider = new ethers.providers.InfuraProvider(sNetwork, "3e7f88ed3dc242c383
 		99b3969b60796ddf89480448423420ee2cee807936867885909508b17d0e635a
 		0x2DFe5B0D283B81AA88D77083C9FBA195B2eF3bA1
 */
-let receiverAddress1 = "0x2DFe5B0D283B81AA88D77083C9FBA195B2eF3bA1";
-let receiverAddress2 = "0x02CC64973a38A82A446A9c4BF3C68c126ecF764d";
+let receiverAddress1 = "0x02CC64973a38A82A446A9c4BF3C68c126ecF764d";
+let receiverAddress2 = "0x23A14e97A59779165BF83310a712B84F101c9140";
 //let amountInEther = "0.001";
 
 let gas_limit = '0x989680'; //5000000 //"0x100000" // Gas Limit & Usage by Txn: 5,000,000 | 21,000 (0.42%) 
@@ -43,10 +43,10 @@ return console.log('##');
 	
 let txFromAddress = receiverAddress1;
 let txToAddress = receiverAddress2;
-let private_key = "99b3969b60796ddf89480448423420ee2cee807936867885909508b17d0e635a";
+let private_key = "31a24edad8548ae2ab963156c2ec15b8480fd66e4c3e39278e2247ed5c6ac035";
 
-let wallet = new ethers.Wallet(private_key)
-let walletSigner = wallet.connect(provider)
+let wallet = new ethers.Wallet(private_key);
+let walletSigner = wallet.connect(provider);
 
 /*
 https://docs.ethers.io/v5/api/utils/transactions/
@@ -54,7 +54,7 @@ https://docs.ethers.io/v5/api/utils/transactions/
 const tx = {
 	from: txFromAddress,
 	//to: txToAddress,
-	//value: 1, //ethers.utils.parseEther(send_token_amount),
+	value: 1, //ethers.utils.parseEther(send_token_amount),
 	nonce: provider.getTransactionCount(
 		txFromAddress,
 		"latest"
