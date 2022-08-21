c6928561f9973a3c8d211ab55a4a6226284ea6f3
Robert-Dan <danrobert445@gmail.com>
2022-08-21 12:53:37 +0000

    👌👌👌

--- src/Suiteziel/End/Transport.php
+++ src/Suiteziel/End/Transport.php
@@ -2,8 +2,6 @@
 namespace App\Suiteziel\End;
 
 
-use Web3\Web3;
-
 class Transport
 {
 
@@ -11,29 +9,57 @@ class Transport
 	}
 
 	public function implement($o = null) {
-		
-		$web3 = new Web3('https://ropsten.infura.io/v3/3e7f88ed3dc242c38332ecf58900a68e');
-		
-		
-    $accounts = $web3->eth()->getBalance('0xe9884777D9F377530788CeE12A40269f812cF30a')->toEth(); // 100
-    var_dump($accounts);
-/*		
-curl https://mainnet.infura.io/v3/3e7f88ed3dc242c38332ecf58900a68e \
--X POST \
--H "Content-Type: application/json" \
--d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
-*/
+
 		#$sPayload = '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}';
 		#$sPayload = '{"method":"parity_enode","params":[],"id":1,"jsonrpc":"2.0"}';
 		
+		// Contract address after transaction
+		$sTransactionHash = "";
+		$sContractAddress = '{"method":"eth_getTransactionReceipt","params":["'.$sTransactionHash.'"],"id":1,"jsonrpc":"2.0"}';
+		#'{"method":"eth_sendTransaction","params":[{"from":"0xb60e8dd61c5d32be8058bb8eb970870f07233155","to":"0xd46e8dd67c5d32be8058bb8eb970870f07244567",
+		#"gas":"0x76c0","gasPrice":"0x9184e72a000","value":"0x9184e72a","data":"0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"}]
+		#,"id":1,"jsonrpc":"2.0"}'
+		$sSendFrom = "0xe9884777D9F377530788CeE12A40269f812cF30a";
+		$sSendTo = ""; #"0xd46e8dd67c5d32be8058bb8eb970870f07244567";
+		$sSendData = "0x600060006009f0"; #0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675
+		$sSendTransaction = '{"method":"eth_sendTransaction",
+		"params":[{
+		"from":"'. $sSendFrom .'",
+		"to":"'. $sSendTo .'",
+		"gas":"0x76c0",
+		"gasPrice":"0x9184e72a000",
+		"value":"0x9184e72a",
+		"data":"'. $sSendData .'"}],
+		"id":1,
+		"jsonrpc":"2.0"}';
 		
-		$sPayload = '{"method":"eth_protocolVersion","params":[],"id":1,"jsonrpc":"2.0"}';
-		#string(40) "{"jsonrpc":"2.0","id":1,"result":"0x41"}"
+		#'{"method":"eth_sendRawTransaction","params":["0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"],"id":1,"jsonrpc":"2.0"}'
+		$sSendFrom = "0xe9884777D9F377530788CeE12A40269f812cF30a";
+		$sSendTo = ""; #"0xd46e8dd67c5d32be8058bb8eb970870f07244567";
+		$sSendData = "0x600060006009f0"; #0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675
+		$sRawTransaction = '{"method":"eth_signTransaction",
+		"params":[{
+		"from":"'. $sSendFrom .'",
+		"to":"",
+		"data":"'. $sSendData .'"}],
+		"id":1,
+		"jsonrpc":"2.0"}';
+
 		
 		
+		#'{"method":"eth_sign","params":["0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826","0x5363686f6f6c627573"],"id":1,"jsonrpc":"2.0"}'
+		$sSendFrom = "0xe9884777D9F377530788CeE12A40269f812cF30a";
+		$sSendTo = ""; #"0xd46e8dd67c5d32be8058bb8eb970870f07244567";
+		$sSendData = "0x600060006009f0"; #0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675
+		$sRawTransaction = '{"method":"eth_sign",
+		"params":[
+			"0xe9884777D9F377530788CeE12A40269f812cF30a",
+			"0x600060006009f0"
+		],
+		"id":1,
+		"jsonrpc":"2.0"}';
 		
-		
-		$sResult = $this->rpc_request($sPayload);
+		$sResult = $this->rpc_request($sRawTransaction);
 		var_dump($sResult);
 		
 		
@@ -64,6 +90,14 @@ curl https://mainnet.infura.io/v3/3e7f88ed3dc242c38332ecf58900a68e \
 		$output = curl_exec($ch);
 		curl_close($ch);
 		return $output;
+	}
+	
+	public function shift_left ($sHex) {
+		return "0x". str_pad($sHex, 64, 0, STR_PAD_LEFT);
+	}
+    		
+	public function shift_right ($sHex) {
+		return "0x". str_pad($sHex, 64, 0, STR_PAD_RIGHT);
 	}
 	
 	private function rpc_client($o){}
