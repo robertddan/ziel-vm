afa4b496b7ecb682a4c7babf23d24700d943d93e
Robert-Dan <danrobert445@gmail.com>
2022-08-21 16:31:28 +0000

    👌👌👌

--- src/Suiteziel/End/Transport.php
+++ src/Suiteziel/End/Transport.php
@@ -1,22 +1,21 @@
 <?php
 namespace App\Suiteziel\End;
 
-use Web3\Web3;
 use kornrunner\Ethereum\Transaction;
 
 class Transport
 {
-
+	private $web3;
+	
+	
 	public function __construct () {
-		$web3 = new Web3('https://ropsten.infura.io/v3/3e7f88ed3dc242c38332ecf58900a68e');
 	}
 
 	public function implement($o = null) {
-
+		#$this->web3 = new Web3('https://ropsten.infura.io/v3/3e7f88ed3dc242c38332ecf58900a68e');
+		
 		#$sPayload = '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}';
 		#$sPayload = '{"method":"parity_enode","params":[],"id":1,"jsonrpc":"2.0"}';
-
-		
 		
 		#'{"method":"eth_sendTransaction","params":[{"from":"0xb60e8dd61c5d32be8058bb8eb970870f07233155","to":"0xd46e8dd67c5d32be8058bb8eb970870f07244567",
 		#"gas":"0x76c0","gasPrice":"0x9184e72a000","value":"0x9184e72a","data":"0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"}]
@@ -67,13 +66,13 @@ class Transport
 											2000000014
 		*/
 		$nonce    = '04';
-		$gasPrice = "0x";
+		$gasPrice = "0xD09DC30E";
 		$gasLimit = "0x989680";
-		$to       = '';
-		$value    = '';
+		$to       = '0x23A14e97A59779165BF83310a712B84F101c9140';
+		$value    = '0x09';
 		$chainId  = 3;
 		$sSendData = "0x600060006009f0";
-		$privateKey = '99b3969b60796ddf89480448423420ee2cee807936867885909508b17d0e635a';
+		$privateKey = '9c20c00af708be5f1870e138d48c96eaa54e5eea406194d9f1405bf3793c488f';
 		#31a24edad8548ae2ab963156c2ec15b8480fd66e4c3e39278e2247ed5c6ac035 
 		#0x02CC64973a38A82A446A9c4BF3C68c126ecF764d
 		#9c20c00af708be5f1870e138d48c96eaa54e5eea406194d9f1405bf3793c488f 
@@ -86,25 +85,25 @@ class Transport
 		$sResultData = $transaction->getRaw ($privateKey, $chainId);
 		// f86d048503f5476a0083027f4b941a8c8adfbe1c59e8b58cc0d515f07b7225f51c72882a45907d1bef7c008025a0db4efcc22a7d9b2cab180ce37f81959412594798cb9af7c419abb6323763cdd5a0631a0c47d27e5b6e3906a419de2d732e290b73ead4172d8598ce4799c13bda69
 		// f850048080808087600060006009f025a0fa47e482938932e7d78da5b267d4bb6b6092ea59fd9784601b484aa292a14444a0521c528e77afffbef7f6838274ab8417cf23081ce1c875d73c717ecc66cc1564
-		var_dump($sResultData);
+		// f853048083989680808087600060006009f029a0679449c12cbd048416454d0fc655708d07dd3e3e922dd2111ad68c5d441a5eaca0511695e61d07818b16af21a0e3088d8f0401381360efef34f632303783022c45
+		#var_dump($sResultData);
 		
 		
 		#'{"method":"eth_sign","params":["0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826","0x5363686f6f6c627573"],"id":1,"jsonrpc":"2.0"}'
 		#$sSendFrom = "0x02CC64973a38A82A446A9c4BF3C68c126ecF764d";
 		#$sSendTo = ""; #"0xd46e8dd67c5d32be8058bb8eb970870f07244567";
-		#$sSendData = "0x600060006008f0"; #0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675
+		#$sSendData = "0x600060006008f0"; #xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675
+		
 		$sRawTransaction = '{"method":"eth_sendRawTransaction",
 		"params":[
 			"0x'. $sResultData .'"
 		],
 		"id":1,
 		"jsonrpc":"2.0"}';
-		
 		$sResultRequest = $this->rpc_request($sRawTransaction);
-		var_dump(["eth_sendRawTransaction: ", $sResultRequest]);
 		
+		var_dump(["eth_sendRawTransaction: ", $sResultRequest]);
 		$fileLog = 'people.log';
-		if (isset($sResultRequest))
 		file_put_contents($fileLog, ($sResultRequest.PHP_EOL), FILE_APPEND | LOCK_EX);
 		
 		
@@ -112,18 +111,13 @@ class Transport
 		// Contract address after transaction
 		$aTransactionHash = json_decode($sResultRequest, true);
 		var_dump($aTransactionHash);
-		$sTransactionHash = $aTransactionHash['result'];
+		$sTransactionHash = "0xeb80b552e9016f70fc38ad0981994e99eba385246eb762073189dc20e4e9b0df"; #$aTransactionHash['result'];
 		#$sTransactionHash = "0x4cf9f9c37424d9287edb61fe64065dfdbefa92e49c78eb5ed0716d844d668759";
 		$sContractAddress = '{"method":"eth_getTransactionReceipt","params":["'. $sTransactionHash .'"],"id":1,"jsonrpc":"2.0"}';
 		$sResultRequest = $this->rpc_request($sContractAddress);
-
+		
 		var_dump(["eth_getTransactionReceipt: ", $sResultRequest]);
-
-
-		
-		
 		$fileLog = 'people.log';
-		if (isset($sResultRequest))
 		file_put_contents($fileLog, ($sResultRequest.PHP_EOL), FILE_APPEND | LOCK_EX);
 		
 		return true;
@@ -153,7 +147,7 @@ class Transport
 		#curl_setopt($ch, CURLOPT_PORT, 30303);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $sPayload);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $aHeaders);
-
+		
 		$output = curl_exec($ch);
 		curl_close($ch);
 		return $output;
@@ -162,7 +156,7 @@ class Transport
 	public function shift_left ($sHex) {
 		return "0x". str_pad($sHex, 64, 0, STR_PAD_LEFT);
 	}
-    		
+	
 	public function shift_right ($sHex) {
 		return "0x". str_pad($sHex, 64, 0, STR_PAD_RIGHT);
 	}
---
+++ www/index.js
@@ -0,0 +1,85 @@
+(async function(){
+
+const ethers = require('ethers');
+const path = require('path')
+const fs = require('fs')
+const ziel = require('./ziel');
+
+	
+let sNetwork = 'ropsten'; //homestead
+let provider = new ethers.providers.InfuraProvider(sNetwork, "3e7f88ed3dc242c38332ecf58900a68e");
+//let wallet = new ethers.Wallet(ziel.sZielPrivate1, provider);
+/*
+		#31a24edad8548ae2ab963156c2ec15b8480fd66e4c3e39278e2247ed5c6ac035 
+		#0x02CC64973a38A82A446A9c4BF3C68c126ecF764d
+		#9c20c00af708be5f1870e138d48c96eaa54e5eea406194d9f1405bf3793c488f 
+		#0x23A14e97A59779165BF83310a712B84F101c9140
+*/
+let receiverAddress1 = "0x02CC64973a38A82A446A9c4BF3C68c126ecF764d";
+let receiverAddress2 = "0x23A14e97A59779165BF83310a712B84F101c9140";
+//let amountInEther = "0.001";
+
+let gas_limit = 5000000 //"0x100000" // Gas Limit & Usage by Txn: 5,000,000 | 21,000 (0.42%) 
+let gas_price = 250000000000 //22500000000 // fast-33 average-30 slow-22.5 // 0.0000001 Ether (100 Gwei) // gas calculator
+
+/*
+Value: 1 wei ($0.00)
+Transaction Fee: 0.0021 Ether ($0.00)
+Gas Price: 0.0000001 Ether (100 Gwei)
+Gas Limit & Usage by Txn: 5,000,000 | 21,000 (0.42%)
+Gas Fees: Base: 0.104296913 Gwei |Max: 100 Gwei |Max Priority: 100 Gwei
+Burnt & Txn Savings Fees: 🔥 Burnt: 0.000002190235173 Ether ($0.00) 💸 Txn Savings: 0 Ether ($0.00)
+*/
+/*
+const sGasPrice = await provider.getGasPrice().then((currentGasPrice) => {
+	//console.log(BigNumber.toString(currentGasPrice));
+	let gas_price = ethers.utils.hexlify(parseInt(currentGasPrice))
+	console.log(`gas_price: ${gas_price}`)
+});
+return console.log('##');
+*/
+	
+let txFromAddress = receiverAddress1;
+let txToAddress = receiverAddress2;
+let private_key = "31a24edad8548ae2ab963156c2ec15b8480fd66e4c3e39278e2247ed5c6ac035";
+
+let wallet = new ethers.Wallet(private_key)
+let walletSigner = wallet.connect(provider)
+
+/*
+https://docs.ethers.io/v5/api/utils/transactions/
+*/
+const tx = {
+	from: txFromAddress,
+	to: txToAddress,
+	value: 1, //ethers.utils.parseEther(send_token_amount),
+	nonce: provider.getTransactionCount(
+		txFromAddress,
+		"latest"
+	),
+	gasLimit: ethers.utils.hexlify(gas_limit),
+	gasPrice: gas_price,
+}
+console.dir(tx)
+
+
+	
+try {
+	walletSigner.sendTransaction(tx)
+	.then(async function(tx){
+		console.log(tx);
+		return tx;
+	})
+	.then(async function(tx){
+		let response = await tx.wait();
+		console.log(response);
+	})
+	.catch(async function(error){
+		console.log(error);
+	});
+} catch (error) {
+	console.log(error);
+}
+	
+})()
+
